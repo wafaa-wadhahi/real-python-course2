45d4eff05e519e10fcc75a054973d31d6dbc9859
wafaa <wafaawadhahi@gmail.com>
2022-03-17 12:10:25 +0300

    show tasks

--- project/views.py
+++ project/views.py
@@ -4,7 +4,7 @@ import sqlite3
 import _config
 from functools import wraps
 from flask import Flask, flash, redirect, render_template, \
-    request, session, url_for
+    request, session, url_for, g
 
 # config
 
@@ -46,3 +46,29 @@ def login():
             flash('Welcome!')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cursor = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=1'
+    )
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+        task_id=row[3]) for row in cursor.fetchall()
+    ]
+    cursor = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=0'
+    )
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+            task_id=row[3]) for row in cursor.fetchall()
+    ]
+    g.db.close()
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
