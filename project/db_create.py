b9bb225f4b6097f5fd29e0f10b2ea90a8a34b2c9
wafaa <wafaawadhahi@gmail.com>
2022-03-20 14:47:21 +0300

    use sqlalchemy for the databases

--- project/db_create.py
+++ project/db_create.py
@@ -1,22 +1,13 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+# create the database and the db table
+db.create_all()
 
-    # get a cursor object to exectue SQL commands
-    c = connection.cursor()
+# insert data
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10,3), 10, 1))
 
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)
-    """)
-
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-        )
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-        )
+# commit the changes
+db.session.commit()
